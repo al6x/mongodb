f9f9d4783c900b703f56a3078484537ec0c8e392
alexeypetrushin <alexey.petrushin@gmail.com>
2011-08-24 03:06:43 +0400

    upd

--- spec/integration/am_validation_spec.rb
+++ spec/integration/am_validation_spec.rb
@@ -1,40 +1,34 @@
 require 'model/spec_helper'
+require 'active_model'
 
-begin
-  require 'active_model'
+describe "Validations" do
+  with_mongo_model
 
-  describe "Validations" do
-    with_mongo_model
+  before do
+    class Unit
+      inherit Mongo::Model
+      collection :units
 
-    before do
-      class Unit
-        inherit Mongo::Model
-        collection :units
+      include ActiveModel::Validations
 
-        include ActiveModel::Validations
+      attr_accessor :name, :status
 
-        attr_accessor :name, :status
-
-        validates_presence_of :name
-      end
-    end
-
-    after{remove_constants :Unit}
-
-    it "ActiveModel integration smoke test" do
-      unit = Unit.new
-      unit.should be_invalid
-      unit.errors.size.should == 1
-      unit.errors.first.first.should == :name
-      unit.save.should be_false
-
-      unit.name = 'Zeratul'
-      unit.should be_valid
-      unit.errors.should be_empty
-      unit.save.should be_true
+      validates_presence_of :name
     end
   end
 
-rescue LoadError => e
-  warn 'No ActiveModel, integration with ActiveModel::Validations spec will be skipped.'
+  after{remove_constants :Unit}
+
+  it "ActiveModel integration smoke test" do
+    unit = Unit.new
+    unit.should be_invalid
+    unit.errors.size.should == 1
+    unit.errors.first.first.should == :name
+    unit.save.should be_false
+
+    unit.name = 'Zeratul'
+    unit.should be_valid
+    unit.errors.should be_empty
+    unit.save.should be_true
+  end
 end
